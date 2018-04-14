cd6e630dbf029b94f7eaa25460b4dc7485ad64e6
Sarika Matthew <pragmattika@Sarika-Matthews-MacBook-Pro-2.local>
2018-04-14 10:10:28 -0400

    start page- images not working

--- client/src/pages/Start/Start.js
+++ client/src/pages/Start/Start.js
@@ -1,45 +1,49 @@
 import React from "react";
 import { Col, Row, Container } from "react-materialize";
+import "./Start.css";
+import tiger from "./tiger.jpg";
+import turtle from "./turtle.jpg";
+import wolf from "./wolf.jpg";
+import polarbear from "./polarbear.jpg";
 
 
 const Start = () =>
 <Container fluid>
-   <Row className = 'slider' >
-       <Col l={12}>
-            <ul className = 'slides'>
-              <li>
-                <img src = 'http://lorempixel.com/580/250/nature/1' /> 
-                        {/* <!-- random image --> */}
-                <div className = 'caption center-align'>
-                  <h3>Post 1</h3>
-                  <h5 className = 'light grey-text text-lighten-3'>Here’s our small slogan.</h5>
-                </div>
+   <Row className = 'slider fullscreen' >
+       <Col>
+        <div className="mainContent">
+              <ul className = 'slides'>
+                <li>
+                  <img src={tiger}  alt={"tiger"} /> 
+                  <div className = 'caption center-align'>
+                    <h3>Post 1</h3>
+                    <h5 className = 'light grey-text text-lighten-3'>Here’s our small slogan.</h5>
+                  </div>
+                </li>
+                <li>
+                  <img src={turtle}  alt={"turtle"}/> 
+                  <div className= "caption center-align">
+                    <h3>Post 2</h3>
+                    <h5 className = "light grey-text text-lighten-3">Here’s our small slogan.</h5>
+                  </div>
+                </li>
+                <li>
+                  <img src={wolf}  alt={"wolf"}/> 
+                          
+                  <div className= "caption center-align">
+                    <h3>Post 3</h3>
+                    <h5 className = "light grey-text text-lighten-3">Here’s our small slogan.</h5>
+                  </div>
+                </li>
+                <li>
+                  <img src={polarbear}  alt={"polarbear"} /> 
+                  <div className= "caption center-align">
+                    <h3>Post 4</h3>
+                    <h5 className="light grey-text text-lighten-3">Here’s our small slogan.</h5>
+  								</div>
               </li>
-              <li>
-                <img src='http://lorempixel.com/580/250/nature/2' /> 
-                        {/* <!-- random image --> */}
-                <div className= "caption center-align">
-                  <h3>Post 2</h3>
-                  <h5 className = "light grey-text text-lighten-3">Here’s our small slogan.</h5>
-                </div>
-              </li>
-              <li>
-                <img src= "http://lorempixel.com/580/250/nature/3" /> 
-                        {/* <!-- random image --> */}
-                <div className= "caption center-align">
-                  <h3>Post 3</h3>
-                  <h5 className = "light grey-text text-lighten-3">Here’s our small slogan.</h5>
-                </div>
-              </li>
-              <li>
-                <img src="http://lorempixel.com/580/250/nature/4" /> 
-                        {/* <!-- random image --> */}
-                <div className= "caption center-align">
-                  <h3>Post 4</h3>
-                  <h5 className="light grey-text text-lighten-3">Here’s our small slogan.</h5>
-								</div>
-            </li>
-					</ul>
+  					</ul>
+          </div>
 				</Col>
 			</Row>
 	</Container>;
