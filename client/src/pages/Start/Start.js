8fecbc42b6860a148fc3f8569bbaea957a30cef4
Sarika Matthew <pragmattika@Sarika-Matthews-MacBook-Pro-2.local>
2018-04-14 11:57:39 -0400

    start images working

--- client/src/pages/Start/Start.js
+++ client/src/pages/Start/Start.js
@@ -1,37 +1,34 @@
 import React from "react";
 import { Col, Row, Container } from "react-materialize";
 import "./Start.css";
-import tiger from "./tiger.jpg";
-import turtle from "./turtle.jpg";
-import wolf from "./wolf.jpg";
-import polarbear from "./polarbear.jpg";
 import Search from "../../components/Search";
 
 
-
 const Start = () =>
+
+
 <Container fluid>
   <Row>
-   <Row className = 'slider fullscreen' >
+   <Row className = 'slider fullscreen' style={{zIndex: "-100"}}>
        <Col>
         <div className="mainContent">
               <ul className = 'slides'>
                 <li>
-                  <img src={tiger}  alt={"tiger"} /> 
+                  <img src="/assets/images/tiger.jpg"  alt="tiger"/> 
                   <div className = 'caption center-align'>
                     <h3>Post 1</h3>
                     <h5 className = 'light grey-text text-lighten-3'>Here’s our small slogan.</h5>
                   </div>
                 </li>
                 <li>
-                  <img src={turtle}  alt={"turtle"}/> 
+                  <img src="/assets/images/turtle.jpg"  alt="turtle"/> 
                   <div className= "caption center-align">
                     <h3>Post 2</h3>
                     <h5 className = "light grey-text text-lighten-3">Here’s our small slogan.</h5>
                   </div>
                 </li>
                 <li>
-                  <img src={wolf}  alt={"wolf"}/> 
+                  <img src="/assets/images/wolf.jpg"  alt="wolf"/> 
                           
                   <div className= "caption center-align">
                     <h3>Post 3</h3>
@@ -39,7 +36,7 @@ const Start = () =>
                   </div>
                 </li>
                 <li>
-                  <img src={polarbear}  alt={"polarbear"} /> 
+                  <img src="/assets/images/polarbear.jpg"  alt="polarbear" /> 
                   <div className= "caption center-align">
                     <h3>Post 4</h3>
                     <h5 className="light grey-text text-lighten-3">Here’s our small slogan.</h5>
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
 app.use(express.static(path.join(__dirname, "client/build/")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Add routes, both API and view
 app.use(routes);
